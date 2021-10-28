446e9cd30cdc6ef66278ede615556fba6e51ef97
hemanth97 <hsai7980@gmail.com>
2021-10-27 19:43:55 -0500

    more changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var alpha
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  alpha = alpha+1
+  res.send('User accesses are: ${alpha}');
+  
 });
 
 module.exports = router;
