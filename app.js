fa168725b1755217209dab3c5954e113510af274
hemanth97 <hsai7980@gmail.com>
2021-10-27 21:02:57 -0500

    bonus changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
-var computation = require('./routes/computation');
+var computationRouter = require('./routes/computation');
 
 var app = express();
 
